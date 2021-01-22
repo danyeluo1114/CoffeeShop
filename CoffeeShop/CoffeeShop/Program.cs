7f8c0ef461e8cd8145d5932f750bbb514fe60943
danyeluo1114 <dxl425@case.edu>
2021-01-22 15:18:29 -0500

    Implement help command

--- CoffeeShop/CoffeeShop/Program.cs
+++ CoffeeShop/CoffeeShop/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoffeeShop.DataAccess.Model;
+using System;
 
 namespace CoffeeShop
 {
@@ -7,6 +8,26 @@ namespace CoffeeShop
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Coffee Shop!");
+            Console.WriteLine("Write 'help' to list available commands");
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available coffee shop commands:");
+                    foreach (var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine($"> " + coffeeShop.Location);
+                    }
+                }
+            }
+
+
         }
     }
 }
