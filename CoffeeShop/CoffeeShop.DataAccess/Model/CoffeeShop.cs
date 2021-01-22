db1a391960bb38b34754a106392fa18ba6d63ae3
danyeluo1114 <dxl425@case.edu>
2021-01-22 15:18:03 -0500

    Add DataAccess project to solution

---
+++ CoffeeShop/CoffeeShop.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,9 @@
+﻿namespace CoffeeShop.DataAccess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+        public int BeanInStockInKg { get; set; }
+
+    }
+}
---
+++ CoffeeShop/CoffeeShop.DataAccess/Model/CoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CoffeeShop.DataAccess.Model
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "Cleveland", BeanInStockInKg = 107 };
+            yield return new CoffeeShop { Location = "Chicago", BeanInStockInKg = 23 };
+            yield return new CoffeeShop { Location = "LA", BeanInStockInKg = 56 };
+        }
+
+    }
+}
